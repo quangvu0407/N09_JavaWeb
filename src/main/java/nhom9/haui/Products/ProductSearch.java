6b60860f9586816bef7630d865f3b5d8e145ea37
quangvu0407 <trantuyenquang0407@gmail.com>
2025-05-05 21:46:13 +0700

    javaweb

---
+++ src/main/java/nhom9/haui/Products/ProductSearch.java
@@ -0,0 +1,57 @@
+package nhom9.haui.Products;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import nhom9.haui.jdbc.ConnectJDBC;
+
+@WebServlet("/ProductSearch")
+public class ProductSearch extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String keyword = request.getParameter("product");
+
+        String sql = "SELECT * FROM Products WHERE name LIKE ? COLLATE utf8mb4_unicode_ci";
+        List<Product> productList = new ArrayList<>();
+
+        try (Connection cnn = new ConnectJDBC().getConnection();
+             PreparedStatement pst = cnn.prepareStatement(sql)) {
+
+            pst.setString(1, "%" + keyword + "%");
+
+            try (ResultSet rs = pst.executeQuery()) {
+                while (rs.next()) {
+                    Product product = new Product(                    
+            	            rs.getInt("category_id"),
+            	            rs.getObject("promotion_id") != null ? rs.getInt("promotion_id") : null,
+            	            rs.getString("name"),
+            	            rs.getString("code"),
+            	            rs.getInt("price"),
+            	            rs.getInt("quantity"),
+            	            rs.getString("thumbnail"),
+            	            rs.getString("description"),
+            	            rs.getString("created_at")
+            	        );
+            	        productList.add(product);
+                }
+            }
+            System.out.println("Danh sách sản phẩm tìm được: " + productList.size());
+            for (Product p : productList) {
+                System.out.println(p.getName());
+            }
+            request.setAttribute("productList", productList);
+            request.getRequestDispatcher("Products/Home.jsp").forward(request, response);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
